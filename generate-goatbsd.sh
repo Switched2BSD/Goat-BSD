ce47e1755c5a85c7d26cd42adfd6092d6879d64f
chris <chrifromgreece@gmail.com>
2023-04-20 21:28:48 +0300

    i'm in the start creating the generator script ! phase 4/20

--- generate-goatbsd.sh
+++ generate-goatbsd.sh
@@ -50,6 +50,9 @@ chroot ~/goat-bsd/core ./freebsd-desktop-installer-git
 clear
 echo " ok installation should be complete now lets compress the whole thing now"
 echo " it will take a LOT OF TIME !!!! ONE HELL OF A LOT OF TIME"
-cd ~/goat-bsd/
-doas nice -20 tar --zstd -cf fullsystem.tar.zst  --options zstd:compression-level=22,zstd:threads=0  ~/goat-bsd/core/
+cd ~/goat-bsd/core
+doas nice -20 tar --zstd -cf ../fullsystem.tar.zst  --options zstd:compression-level=15,zstd:threads=0  .
+echo "Compression Finished !"
+
+
 
